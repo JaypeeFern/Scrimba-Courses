a18f5ab6f9cf03b2d646909440ed9b5b327b4185
JaypeeFern <103358200+JaypeeFern@users.noreply.github.com>
2023-03-10 03:18:50 +0800

    added styling and header component

--- Assessment/1st-assessment/src/App.jsx
+++ Assessment/1st-assessment/src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <div className="App">
-
+    <div className="main-container">
+      <Header />
+      <main className="body--container">
+   
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Assessment/1st-assessment/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="navbar--container">
+      <span className='navbar--textleft'>Group XS</span>
+      <span className='navbar--textright'>Assessment 1</span>
+    </div>
+  )
+}
