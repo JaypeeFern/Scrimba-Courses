b1b9eedf51dc6e3d1d67ae056934c252bf40f9dd
JaypeeFern <103358200+JaypeeFern@users.noreply.github.com>
2023-03-10 14:02:13 +0800

    restructured DOM elements and css

--- Assessment/1st-assessment/src/App.jsx
+++ Assessment/1st-assessment/src/App.jsx
@@ -7,10 +7,19 @@ function App() {
     <div className="main-container">
       <Header />
       <main className="body--container">
-   
+          <div className='body--left'>
+          
+          </div>
+          <div className='body--right'>
+          
+          </div>
       </main>
     </div>
   );
 }
 
 export default App;
+
+// https://helios-i.mashable.com/imagery/articles/05fQvWTQeAAdXYZNNst8ukj/hero-image.fill.size_1200x1200.v1619479187.png
+// https://mashable.com/article/best-free-recipe-apps
+// https://www.google.com/search?q=Recipe+Book+App&sxsrf=AJOqlzWuRYZUzfWZIONXd2Xeu-trWqY0Mw:1678425958971&source=lnms&tbm=isch&sa=X&ved=2ahUKEwihv8P4z9D9AhWomlYBHUMPAPQQ_AUoAXoECAEQAw&biw=1366&bih=636&dpr=1#imgrc=RNDiKYH7mXC_cM
--- Assessment/1st-assessment/src/components/Footer.jsx
+++ Assessment/1st-assessment/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer--container">
-
+      <span className='footer--text'>Created for assessment purposes only. All Rights Reserved</span>
     </div>
   )
 }
