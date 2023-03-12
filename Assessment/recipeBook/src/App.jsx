1b04537c43bd99363c10722529356dabf1f45990
JaypeeFern <103358200+JaypeeFern@users.noreply.github.com>
2023-03-12 22:08:58 +0800

    added update functionality

--- Assessment/recipeBook/src/App.jsx
+++ Assessment/recipeBook/src/App.jsx
@@ -2,120 +2,174 @@ import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Dish from './components/Dish'
 import AddDish from "./forms/AddDish";
+import UpdateDish from "./forms/UpdateDish";
+import Forms from "./forms/Forms";
 import { nanoid } from 'nanoid'
+
 
 function App() {
 
-    // Get the data from Local Storage
-    const getFood = () => JSON.parse(localStorage.getItem('food')) || []
-    // Set the data to the state
-    const [food, setFood] = React.useState(getFood)
-    // Create state for dish ID
-    const [currentFoodId, setCurrentFoodId] = React.useState((food[0] && food[0].id) || '')
-    // Add the data to the Local Storage
-    localStorage.setItem('food', JSON.stringify(food))
-    // Everytime the state changes, the data in the Local Storage will be updated
-    React.useEffect(() => {
-      const getFood = JSON.parse(localStorage.getItem('food'))
-      setFood(getFood)
-    }, [currentFoodId])
+  // Get the data from Local Storage
+  const getFood = () => JSON.parse(localStorage.getItem('food')) || []
+  // Set the data to the state
+  const [food, setFood] = React.useState(getFood)
+  // Create state for dish ID
+  const [currentFoodId, setCurrentFoodId] = React.useState((food[0] && food[0].id) || '')
+  // Add the data to the Local Storage
+  localStorage.setItem('food', JSON.stringify(food))
+  // Everytime the state changes, the data in the Local Storage will be updated
+  React.useEffect(() => {
+    const getFood = JSON.parse(localStorage.getItem('food'))
+    setFood(getFood)
+  }, [currentFoodId])
 
   // Random Food Image
-    const [imageUrl, setImageUrl] = React.useState('');
-    
-    // API Key for Pexels
-    // TmvsB31UhnWsaSc1eweoin54jJkU8D59cu4UEdNuGmFYtQCiqAdoGdqh
-    
-    useEffect(() => {
-      const randomNumber = Math.floor(Math.random() * 25) + 1;
-      const fetchImage = async () => {
-        const response = await fetch(`https://api.pexels.com/v1/search?query=dish%20presentation%20close%20up&w=1366&h=200&page=${randomNumber}&per_page=10`, {
-          headers: {
-            Authorization: 'TmvsB31UhnWsaSc1eweoin54jJkU8D59cu4UEdNuGmFYtQCiqAdoGdqh'
-          }
-        });
-        const data = await response.json();
-        const randomIndex = Math.floor(Math.random() * data.photos.length);
-        setImageUrl(data.photos[randomIndex].src.large);
+  const [imageUrl, setImageUrl] = React.useState('');
 
-        const remainingRequests = response.headers.get('X-Ratelimit-Remaining');
-        console.log(`Remaining requests: ${remainingRequests}`);
-      };
-      fetchImage();
-    }, [currentFoodId]);
+  // API Key for Pexels
+  // TmvsB31UhnWsaSc1eweoin54jJkU8D59cu4UEdNuGmFYtQCiqAdoGdqh
 
-    // Create a new dish
-    function createNewFood(event) {
-      event.preventDefault()
-      const newFood = { // Create a new dish object
-        id: nanoid(),
-        foodName: event.target.foodName.value,
-        foodDescription: event.target.foodDescription.value,
-        foodImage: imageUrl
-      }
-      // Add the new dish to the state array if the recipe name is not empty
-      if (newFood.foodName !== '') {
-        setFood(prevFood => [...prevFood, newFood])
-      } else {
-        alert('Please enter dish name')
-      }
-      // Set the new dish as the current recipe ID
-      setCurrentFoodId(newFood.id)
+  useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * 25) + 1;
+    const fetchImage = async () => {
+      const response = await fetch(`https://api.pexels.com/v1/search?query=dish%20presentation%20close%20up&w=1366&h=200&page=${randomNumber}&per_page=10`, {
+        headers: {
+          Authorization: 'TmvsB31UhnWsaSc1eweoin54jJkU8D59cu4UEdNuGmFYtQCiqAdoGdqh'
+        }
+      });
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.photos.length);
+      setImageUrl(data.photos[randomIndex].src.large);
+    };
+    fetchImage();
+  }, [currentFoodId]);
+
+  // Create a new dish
+  function createNewFood(event) {
+    event.preventDefault()
+    const newFood = { // Create a new dish object
+      id: nanoid(),
+      foodName: event.target.foodName.value,
+      foodDescription: event.target.foodDescription.value,
+      foodImage: imageUrl
     }
-
-    // Delete a recipe
-    function deleteFood(event, foodId) {
-      event.stopPropagation()
-      // Filter the recipe array and return a new array without the dish that has the same ID as the recipe ID that was passed in
-      setFood(prevFoods => prevFoods.filter(prevFood => prevFood.id !== foodId))
+    // Add the new dish to the state array if the dish name is not empty
+    if (newFood.foodName !== '') {
+      setFood(prevFood => [...prevFood, newFood])
+    } else {
+      alert('Please enter dish name')
     }
-
-    // Update a recipe
-    function updateFood(event, foodId, foodName, foodDescription) {
-      event.stopPropagation()
-      // Store data in an array
-      const updatedFood = {
-        id: foodId,
-        foodName: foodName,
-        foodDescription: foodDescription,
-      }
-      console.log(updatedFood)
-    }
-
-    // Map through the recipe array and return a Food component for each Dish
-    const dishElements = food.map(food => {
-      return (
-        <Dish
-          key={food.id}
-          currentFoodId={food.id}
-          foodName={food.foodName}
-          foodDescription={food.foodDescription}
-          foodImage={food.foodImage}
-          deleteFood={deleteFood}
-          updateFood={updateFood}
-        />
-      )
-    })
-
-    return (
-      <div className="main-container">
-        <Header />
-        <main className="body--container">
-          <div className='body--left'>
-            {dishElements}
-          </div>
-          <div className='body--right'>
-            <AddDish
-              createNewFood={createNewFood}
-            />
-          </div>
-        </main>
-      </div>
-    );
+    // Set the new dish as the current dish ID
+    setCurrentFoodId(newFood.id)
   }
 
-  export default App;
+  // Delete a dish
+  function deleteDish(event, foodId) {
+    event.preventDefault()
+    // Filter the dish array and return a new array without the dish that has the same ID as the dish ID that was passed in
+    setFood(prevFoods => prevFoods.filter(prevFood => prevFood.id !== foodId))
+  }
 
-// https://helios-i.mashable.com/imagery/articles/05fQvWTQeAAdXYZNNst8ukj/hero-image.fill.size_1200x1200.v1619479187.png
-// https://mashable.com/article/best-free-recipe-apps
-// https://www.google.com/search?q=Recipe+Book+App&sxsrf=AJOqlzWuRYZUzfWZIONXd2Xeu-trWqY0Mw:1678425958971&source=lnms&tbm=isch&sa=X&ved=2ahUKEwihv8P4z9D9AhWomlYBHUMPAPQQ_AUoAXoECAEQAw&biw=1366&bih=636&dpr=1#imgrc=RNDiKYH7mXC_cM
+  // Create state for the forms to show or hide
+  const [showAddForm, setShowForm1] = React.useState(false);
+  const [showUpdateForm, setShowUpdateForm] = React.useState(false);
+
+  // Show the add form
+  function handleShowAddForm(event) {
+    event.preventDefault()
+    showAddForm(true);
+    setShowUpdateForm(false);
+  }
+
+  // Show the update form
+  function handleShowUpdateForm(event) {
+    event.preventDefault()
+    setShowForm1(false)
+    setShowUpdateForm(true)
+  }
+
+  // Update a dish
+  function updateDish(event, foodId) {
+    event.preventDefault()
+    handleShowUpdateForm(event)
+    // Find the object with the matching ID
+    const selectedFood = food.find(food => food.id === foodId);
+    if (selectedFood) {
+      // Populate the form with the data from the selected object
+      document.getElementById('foodName').value = selectedFood.foodName;
+      document.getElementById('foodDescription').value = selectedFood.foodDescription;
+
+      // Add an event listener to the form submit button
+      document.getElementById('updateDish').addEventListener('click', function (event) {
+
+        // Get the new values from the form
+        const updatedFood = {
+          foodName: document.getElementById('foodName').value,
+          foodDescription: document.getElementById('foodDescription').value,
+        };
+
+        // Find the index of the object with the matching ID
+        const index = food.findIndex(food => food.foodId === updatedFood.foodId);
+
+        if (index >= 0) {
+          // Modify the existing object with the new values
+          food[index].foodName = updatedFood.foodName;
+          food[index].foodDescription = updatedFood.foodDescription;
+
+          // Update the data in local storage
+          localStorage.setItem('food', JSON.stringify(food));
+
+          console.log(food[index]);
+        } else {
+          console.log(`Food with ID ${updatedFood.foodId} not found.`);
+        }
+      });
+    } else {
+      console.log(`Food with ID ${foodId} not found.`);
+    }
+  }
+
+  function updateDishWithForm(event, foodId) {
+    handleShowUpdateForm(event);
+    setTimeout(() => {
+      updateDish(event, foodId);
+    } , 200)
+  }
+
+  // Map through the dish array and return a Food component for each Dish
+  const dishElements = food.map(food => {
+    return (
+      <Dish
+        key={food.id}
+        currentFoodId={food.id}
+        foodName={food.foodName}
+        foodDescription={food.foodDescription}
+        foodImage={food.foodImage}
+        deleteDish={deleteDish}
+        updateDishWithForm={updateDishWithForm}
+      />
+    )
+  })
+
+  return (
+    <div className="main-container">
+      <Header />
+      <main className="body--container">
+        <div className='body--left'>
+          {dishElements}
+        </div>
+        <div className='body--right'>
+          <Forms
+            createNewFood={createNewFood}
+            showAddForm={showAddForm}
+            handleShowAddForm={handleShowAddForm}
+            showUpdateForm={showUpdateForm}
+            handleShowUpdateForm={handleShowUpdateForm}
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- Assessment/recipeBook/src/forms/AddDish.jsx
+++ Assessment/recipeBook/src/forms/AddDish.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function Create(props) {
+export default function Create({createNewFood}) {
   return (
     <div className="form--container">
-      <form className="form" onSubmit={(event) => props.createNewFood(event)}>
+      <form className="form" onSubmit={(event) => createNewFood(event)}>
         <div className="mb-3 w-75">
           <label htmlFor="foodName">Dish Name</label>
-          <input type="text" className="form-control" id="foodName" value={props.foodName}/>
+          <input type="text" className="form-control" id="foodName"/>
         </div>
         <div className="mb-3 w-75">
           <label htmlFor="foodDescription">Dish Description</label>
           <textarea className="form-control" id="foodDescription" />
         </div>
-        <button type="submit" className="btn btn-primary" id="addDish">Add New Food</button>
+        <button type="submit" className="btn btn-primary" id="addDish">Add New Dish</button>
       </form>
     </div>
   );
