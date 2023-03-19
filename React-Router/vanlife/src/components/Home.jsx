9d58585304602aad46490f1734aa1b841008dac4
JaypeeFern <103358200+JaypeeFern@users.noreply.github.com>
2023-03-20 01:15:58 +0800

    added link to home button

--- React-Router/vanlife/src/components/Home.jsx
+++ React-Router/vanlife/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
@@ -7,7 +8,7 @@ export default function Home() {
                 <div className='home--content'>
                     <span className='home--title'>You got the travel plans, we got the travel vans.</span>
                     <span className='home--desc'>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</span>
-                    <button className='home--btn'>Find your van</button>
+                    <Link to='/vans' className='home--btn'>Find your van</Link>
                 </div>
             </div>
         </div>
