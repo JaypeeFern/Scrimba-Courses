762617ba35c0dde0d8e1160459c9b0be5b1207cc
JaypeeFern <103358200+JaypeeFern@users.noreply.github.com>
2023-03-26 00:39:03 +0800

    refactored to using actions to handle forms

--- React-Router/vanlife/src/components/User/Login.jsx
+++ React-Router/vanlife/src/components/User/Login.jsx
@@ -1,56 +1,51 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useActionData, Form } from "react-router-dom";
 import { loginUser } from "../../API";
 
+export async function action({ request }) {
+    const formData = await request.formData();
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    try {
+        const login = await loginUser({ email, password })
+        localStorage.setItem('loggedin', true)
+        return login
+    } catch (err) {
+        return { error: err.message }
+    }
+
+}
 
 export default function Login() {
 
-    const [formData, setFormData] = React.useState({ email: '', password: '' })
-    const [status, setStatus] = React.useState('idle')
-    const [error, setError] = React.useState(null)
+    const data = useActionData()
     const location = useLocation()
     const navigate = useNavigate()
 
-    async function handleForm(event) {
-        event.preventDefault()
-        setStatus('submitting')
-        setError(null)
-        const response = await loginUser(formData)
-            .then(data => {
-                localStorage.setItem("loggedin", true)
-                navigate(location.state?.from || '/host', { replace: true })
-            })
-            .catch(err => {
-                setError(err)
-            })
-            .finally(() => {
-                setStatus('idle')
-            })
-       
-        return response
+    const isLoggedIn = localStorage.getItem('loggedin')
+    const logOut = () => {
+        return localStorage.removeItem('loggedin')
     }
 
-    function handleChange(event) {
-        const { name, value } = event.target
-        setFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
-
-    // localStorage.removeItem("loggedin")
+    React.useEffect(() => {
+        if (data?.token) {
+            navigate(location.state?.from || '/host', { replace: true })
+        }
+    }, [data])
 
     return (
         <div className="login--container">
             <div className="login--wrapper">
                 <h1>{location.state?.message || null}</h1>
                 <h1 className="login--header">Sign in to your account</h1>
-                {error && <h6 style={{marginTop: '10px', color: 'red', fontWeight: 'bold'}}>{error.message}</h6>}
-                <form onSubmit={handleForm} className="login--form">
-                    <input onChange={handleChange} name="email" placeholder="Email" type='email' value={formData.email} />
-                    <input onChange={handleChange} name="password" placeholder="Password" type='password' value={formData.password} />
-                    <button disabled={status == 'submitting' ? true:false} className={`${status != 'submitting' ? 'login--btn':'login--btn-disabled'}`}></button>
-                </form>
+                {data?.error && <h6 style={{ marginTop: '10px', color: 'red', fontWeight: 'bold' }}>{data.error}</h6>}
+                <Form method="POST" className="login--form">
+                    <input name="email" placeholder="Email" type='email' />
+                    <input name="password" placeholder="Password" type='password' />
+                    <button className={`login--btn`}></button>
+                    {isLoggedIn && <button onClick={logOut}>Log Out</button>}
+                </Form>
             </div>
         </div>
     )
@@ -60,3 +55,16 @@ export default function Login() {
 // if(data.user.id == location.state?.token) {
 //    navigate('/host', { replace: true, state: { token: data.user.id } })
 // }
+
+{/* <div className="login--container">
+<div className="login--wrapper">
+    <h1>{location.state?.message || null}</h1>
+    <h1 className="login--header">Sign in to your account</h1>
+    {error && <h6 style={{marginTop: '10px', color: 'red', fontWeight: 'bold'}}>{error.message}</h6>}
+    <form onSubmit={handleForm} className="login--form">
+        <input onChange={handleChange} name="email" placeholder="Email" type='email' value={formData.email} />
+        <input onChange={handleChange} name="password" placeholder="Password" type='password' value={formData.password} />
+        <button disabled={status == 'submitting' ? true:false} className={`${status != 'submitting' ? 'login--btn':'login--btn-disabled'}`}></button>
+    </form>
+</div>
+</div> */}
